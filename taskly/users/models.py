410ed7d0df07e60a5f2d1a7e7317c9110558418f
wilsonmike <michaelwilsonsj@hotmail.com>
2021-07-21 12:08:11 -0400

    added profile image path || added profile to users models

--- taskly/users/models.py
+++ taskly/users/models.py
@@ -1,5 +1,24 @@
+import os
+
 from django.db import models
 from django.contrib.auth.models import User
+
+from django.utils.deconstruct import deconstructible
 # Create your models here.
+
+@deconstructible
+class GenerateProfileImagePath(object):
+    def __init__(self):
+        pass
+    
+    def __call__(self, instance, filename):
+        ext = filename.split('.')[-1]
+        path = f'media/accounts/{instance.user.id}/images/'
+        name = f'profile_image.{ext}'
+        return os.path.join(path, name)
+
+user_profile_image_path = GenerateProfileImagePath()    
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to=user_profile_image_path)
