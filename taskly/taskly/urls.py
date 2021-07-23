a80cddf6b92c7352efecbb962a49ee17ce00494d
wilsonmike <michaelwilsonsj@hotmail.com>
2021-07-23 16:02:38 -0400

    added api url patterns - api is now consumable @ /api/accounts/users

--- taskly/taskly/urls.py
+++ taskly/taskly/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from users import router as users_api_router
+
+api_url_patters = [
+    path('accounts/', include(users_api_router.router.urls))
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(api_url_patters))
 ]
